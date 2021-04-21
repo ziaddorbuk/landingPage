b760eaad7d164d979baed2ede53f7459985f4949
ziaddorbuk <49367279+ziaddorbuk@users.noreply.github.com>
2021-04-21 05:00:47 +0200

    final version

--- js/app.js
+++ js/app.js
@@ -12,13 +12,13 @@
  * JS Standard: ESlint
  * 
 */
-
 /**
  * Define Global Variables
  * 
 */
-
-
+const sections= Array.from(document.querySelectorAll('section'));
+const list =document.getElementById('navbar__list');
+let counter=sections.length;
 /**
  * End Global Variables
  * Start Helper Functions
@@ -27,29 +27,48 @@
 
 
 
+function isViewed(section){
+    let position = section.getBoundingClientRect();
+    return (position.top>=0);
+}
+
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
-*/
-
-// build the nav
-
-
+ */
+//create navBar
+function newItem(){
+    for(section of sections){
+        sectionName = section.getAttribute('data-nav');
+        link = section.getAttribute('id');
+        item = document.createElement('li');
+        item.innerHTML=`<a class='menu__link'href='#${link}'>${sectionName}</a>`;
+        list.appendChild(item);
+    }
+}
 // Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
+function activeSection(){
+    for(section of sections){
+        if(isViewed(section)){
+            if(!section.classList.contains('your-active-class')){
+                section.classList.add('your-active-class');
+            }
+            else {
+                section.classList.remove('your-active-class');
+            }
+        }
+    }
+}
 
 /**
  * End Main Functions
  * Begin Events
  * 
 */
+// Set sections as active
+document.addEventListener('scroll',activeSection);
 
 // Build menu 
+newItem();
 
-// Scroll to section on link click
-
-// Set sections as active
---
+++ js/readme.md
@@ -0,0 +1,3 @@
+#Landing Page Project
+This project is aboout landing page that contains multiple sections and to navigate 
+between them using navBar
