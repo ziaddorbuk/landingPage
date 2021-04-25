6b68ac8b701a445db4968e075cc2d54bf0de4369
ziaddorbuk <49367279+ziaddorbuk@users.noreply.github.com>
2021-04-25 03:43:26 +0200

    final

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ let pageSetions = Array.from(document.querySelectorAll('section'));
 let list = document.getElementById('navbar__list');
 let i = 0;
 let numOfSections = pageSetions.length;
+
 /************ main functions ************/
  function render(){
     while(i<pageSetions.length){
@@ -10,26 +11,75 @@ let numOfSections = pageSetions.length;
         sectionName = pageSetions[i].getAttribute('data-nav');
         sectionLink = pageSetions[i].getAttribute('id');
         li = document.createElement('li');
-        li.innerHTML = `<a class='menu__link'href='#${sectionLink}'>${sectionName}</a>`;
+        li.innerHTML = `<a class='menu__link' href='#${sectionLink}'>${sectionName} </a>`;
+        // onclick='funcClick(sectionLink)
+        console.log(li);
         list.appendChild(li);
         i++;
-    }   
-}
-function active(){
-    let i2=0;
-    while(i2<numOfSections){
-        if((pageSetions[i2].getBoundingClientRect().top)>=0){
-            if(pageSetions[i2].classList.contains('your-active-class')){
-                pageSetions[i2].classList.remove('your-active-class');
-            }else if(!pageSetions[i2].classList.contains('your-active-class')){
-                pageSetions[i2].classList.add('your-active-class');
-            }   
-        }
-        i2++;
-    }
+    }  
+     
 }
 /************ calling main functions ************/
 // call render fucntion that creates the nav bar
 render();
 /************ events ************/
-document.addEventListener('scroll',active);
+/************ scroll event ************/
+document.addEventListener("scroll", function(){
+    if(!isNaN(delay)){ 
+        clearTimeout(delay); 
+    }
+    delay = setTimeout(checkVisibleSection, 100);
+});
+
+//---Check the visible section
+function checkVisibleSection(){
+
+    var minor   = window.innerHeight,
+        section = null;
+
+    //---Select the section closest to the top
+    [].forEach.call(sections, function(item){
+
+        var offset  = item.getBoundingClientRect();
+
+        if(Math.abs(offset.top) < minor){
+
+            minor   = Math.abs(offset.top);
+
+            section = item;
+
+        }
+
+    });
+
+    //---If the section exists
+    if(section){
+        if(!section.classList.contains("your-active-class")){
+            //---Remove all active classes
+            sections.forEach(sec => {
+                sec.classList.remove('your-active-class');  
+            });
+            //---Add the active class
+            section.classList.add('your-active-class');
+            
+        }
+
+    }
+
+}
+var nav         = document.getElementById("navigation"),
+    sections    = document.querySelectorAll(".section"),
+    delay       = null;
+/************ nav bar on click event ************/
+/* ________ smooth scrolling using JQUERY ________ */
+//on click function that overrides the defult scrolling behavior
+$('.navbar__menu a').on('click',function(e){
+    if(this.hash!==''){
+        e.preventDefault();
+        const hash = this.hash;
+        $('html,body').animate({
+            scrollTop:$(hash).offset().top
+        },800);
+    }
+});
+
